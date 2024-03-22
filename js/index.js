c2d96e6c720a4c373b22e39b80d8e2fbc2696263
BlackJackMC <duytu85343@gmail.com>
2024-03-22 10:06:44 +0700

    Add outline

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
-const heading = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+function addID() {
+    const heading = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
 
+    heading.forEach((item) => {
+        const id = item.innerHTML.trim().replace(/ /g, "-");
+        item.setAttribute("id", id);
+    })
+}
+
+
+hljs.highlightAll();
+addID();
+tocbot.init({
+    tocSelector: ".toc",
+    contentSelector: ".markdown-body",
+    headingSelector: "h1, h2, h3, h4, h5, h6",
+    hasInnerContainers: true,
+    orderedList: false,
+});
